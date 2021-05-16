de2c862c1d99e6261fc06e0f04c8afb57553218c
Peter Baca <bacik18@yahoo.com>
2021-05-16 12:04:42 +0200

    Removed unused import statements in CitatyController.

--- src/main/java/cz/czechitas/java2webapps/ukol6/controller/CitatyController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol6/controller/CitatyController.java
@@ -7,9 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
-import java.util.Random;
-
 /**
  *
  */
